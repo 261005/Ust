77980bebbaf76cdfe85ee6c16008a66f342e55b7
261005 <167771814+261005@users.noreply.github.com>
2024-05-02 19:13:11 +0530

    Add files via upload

---
+++ testFilename.java
@@ -0,0 +1,21 @@
+package webdrivertest;
+
+import utils.fileNameMaker;
+
+public class testFilename {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		fileNameMaker fn = new fileNameMaker();
+		String currfilename = fn.fileNameForLogin("abc", "def");
+		
+		System.out.println(currfilename);
+		
+
+	}
+
+
+}
